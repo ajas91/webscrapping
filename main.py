c576fda27ff0529b8d1083e2d9cf0ffc7a975b3c
Ali Sultan <ajasultan@gmail.com>
2022-03-22 17:09:21 +0400

    add csv extraction

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 import time
+import pandas as pd
 import os
 
 
@@ -22,16 +23,17 @@ import os
 
 
 def getRestaurantData():
+    clean = re.compile('<.*?>')
     reviewBtn = driver.find_elements(by=By.CLASS_NAME, value ='btn')
     reviewBtn[2].click()
 
-    time.sleep(2)
-    for i in range(4):
+    time.sleep(3)
+    for i in range(5):
         driver.current_url
         readMore = driver.find_elements(by=By.CLASS_NAME, value ='btn-block')
         if readMore:
             driver.find_elements(by=By.CLASS_NAME, value ='btn-block')[0].click()
-            time.sleep(2)
+            time.sleep(1)
         else:
             break
 
@@ -39,9 +41,10 @@ def getRestaurantData():
     soup = bs.BeautifulSoup(html_source, features="html.parser")
     comments = []
     for tag in soup.find_all('p',class_="f-500"):
-        comments.append(tag)
+        comments.append(re.sub(clean,'',str(tag)))
+        # comments.append(tag)
     
-    time.sleep(2)
+    time.sleep(1)
     # driver.find_elements(by=By.XPATH,value='//a[@href="/oman/restaurants/1415"]')[0].click()
     # time.sleep(2)
     return comments
@@ -52,20 +55,24 @@ if __name__ == "__main__":
     driver = webdriver.Chrome(executable_path=driverPath)
     site = 'https://www.talabat.com/oman/restaurants/1415/al-mawalih-north'
     driver.get(site)
-    time.sleep(2)
+    time.sleep(1)
     restButtons = driver.find_elements(by=By.CLASS_NAME, value ='restuarant-item')
     comments = []
 
     for i in range(len(restButtons)):
         driver.get(site)
-        time.sleep(2)
+        time.sleep(1)
         restButtons = driver.find_elements(by=By.CLASS_NAME, value ='restuarant-item')
         restButtons[i].click()
-        time.sleep(2)
+        time.sleep(3)
         temp = getRestaurantData()
         for c in temp:
             comments.append(c)
 
-    time.sleep(2)
+    time.sleep(1)
+    s = pd.Series(comments)
+    s = s.str.strip()
+    path = os.path.abspath(os.path.curdir)
+    s.to_csv(os.path.join(path,'comments.csv'))
     print(comments)
     driver.close()
