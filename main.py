709cf9d57e2c70e3d4f59aa9137de2d53d386ec4
Ali Sultan <ajasultan@gmail.com>
2022-03-23 09:16:55 +0400

    look into more pages, fix bug of read more button not available

--- main.py
+++ main.py
@@ -25,49 +25,53 @@ import os
 def getRestaurantData():
     clean = re.compile('<.*?>')
     reviewBtn = driver.find_elements(by=By.CLASS_NAME, value ='btn')
-    reviewBtn[2].click()
+    if len(reviewBtn)>=3:
+        reviewBtn[2].click()
 
-    time.sleep(3)
-    for i in range(5):
-        driver.current_url
-        readMore = driver.find_elements(by=By.CLASS_NAME, value ='btn-block')
-        if readMore:
-            driver.find_elements(by=By.CLASS_NAME, value ='btn-block')[0].click()
-            time.sleep(1)
-        else:
-            break
+        time.sleep(3)
+        for i in range(6):
+            driver.current_url
+            readMore = driver.find_elements(by=By.CLASS_NAME, value ='btn-block')
+            if readMore:
+                driver.find_elements(by=By.CLASS_NAME, value ='btn-block')[0].click()
+                time.sleep(1)
+            else:
+                break
 
-    html_source = driver.page_source
-    soup = bs.BeautifulSoup(html_source, features="html.parser")
-    comments = []
-    for tag in soup.find_all('p',class_="f-500"):
-        comments.append(re.sub(clean,'',str(tag)))
-        # comments.append(tag)
-    
-    time.sleep(1)
-    # driver.find_elements(by=By.XPATH,value='//a[@href="/oman/restaurants/1415"]')[0].click()
-    # time.sleep(2)
-    return comments
+        html_source = driver.page_source
+        soup = bs.BeautifulSoup(html_source, features="html.parser")
+        comments = []
+        for tag in soup.find_all('p',class_="f-500"):
+            comments.append(re.sub(clean,'',str(tag)))
+            # comments.append(tag)
+        
+        time.sleep(1)
+        # driver.find_elements(by=By.XPATH,value='//a[@href="/oman/restaurants/1415"]')[0].click()
+        # time.sleep(2)
+        return comments
+    else:
+        return []
 
 
 if __name__ == "__main__":
     driverPath = os.path.join("C:\\Users\\ajasu\\Downloads","chromedriver.exe")
     driver = webdriver.Chrome(executable_path=driverPath)
-    site = 'https://www.talabat.com/oman/restaurants/1415/al-mawalih-north'
-    driver.get(site)
-    time.sleep(1)
-    restButtons = driver.find_elements(by=By.CLASS_NAME, value ='restuarant-item')
-    comments = []
-
-    for i in range(len(restButtons)):
+    for i in range(7):
+        site = f'https://www.talabat.com/oman/restaurants/1415/al-mawalih-north?page={i+1}'
         driver.get(site)
         time.sleep(1)
         restButtons = driver.find_elements(by=By.CLASS_NAME, value ='restuarant-item')
-        restButtons[i].click()
-        time.sleep(3)
-        temp = getRestaurantData()
-        for c in temp:
-            comments.append(c)
+        comments = []
+
+        for i in range(len(restButtons)):
+            driver.get(site)
+            time.sleep(1)
+            restButtons = driver.find_elements(by=By.CLASS_NAME, value ='restuarant-item')
+            restButtons[i].click()
+            time.sleep(3)
+            temp = getRestaurantData()
+            for c in temp:
+                comments.append(c)
 
     time.sleep(1)
     s = pd.Series(comments)
